3eb5b80d0e89c7de31c8fc2021f4d0e5182d3f29
dixond1097 <79214021+dixond1097@users.noreply.github.com>
2021-05-29 12:29:25 -0400

    MY COMMITS

--- week1/javascript/vending-js/jest.config.js
+++ week1/javascript/vending-js/jest.config.js
@@ -2,8 +2,9 @@
  * For a detailed explanation regarding each configuration property, visit:
  * https://jestjs.io/docs/configuration
  */
+import type { InititalOptionTsJest } from 'ts-jest/dist/types';
 
-module.exports = {
+const config = {
   // All imported modules in your tests should be mocked automatically
   // automock: false,
 
@@ -60,7 +61,11 @@ module.exports = {
   // globalTeardown: undefined,
 
   // A set of global variables that need to be available in all test environments
-  // globals: {},
+  globals: {
+    'ts-jest' : {
+
+        },
+  },
 
   // The maximum amount of workers used to run your tests. Can be specified as % or a number. E.g. maxWorkers: 10% will use 10% of your CPU amount + 1 as the maximum worker number. maxWorkers: 2 will use a maximum of 2 workers.
   // maxWorkers: "50%",
@@ -192,3 +197,4 @@ module.exports = {
   // Whether to use watchman for file crawling
   // watchman: true,
 };
+export default config;
--- week1/javascript/vending-js/src/services/log.ts
+++ week1/javascript/vending-js/src/services/log.ts
@@ -1,4 +1,4 @@
-import log4js from 'log4js';
+import log4js from '../log4js';
 
 log4js.configure('log4js.json');
 const log = log4js.getLogger();
