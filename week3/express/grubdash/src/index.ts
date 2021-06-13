0cf32cd0729e67df077864706112cb3fb317731a
dixond1097 <79214021+dixond1097@users.noreply.github.com>
2021-06-13 17:52:55 -0400

    Index file

--- week3/express/grubdash/src/index.ts
+++ week3/express/grubdash/src/index.ts
@@ -2,65 +2,6 @@ import app from './app';
 
 const port = process.env.PORT || 3000;
 
-import expressSession from 'express-session';
-import express, { NextFunction, Request, Response } from 'express';
-import 'express-async-errors';
-import path from 'path';
-import StatusCodes from 'http-status-codes';
-
-import dotenv from 'dotenv';
-
-import log from './log';
-import baseRouter from './routes';
-
-
-dotenv.config({});
-
-const app = express();
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, './public')));
-app.use(expressSession({
-  secret: 'whatever-probably-should-be-from-env-vars',
-  cookie: {},
-}));
-
-const port = process.env.PORT || 3000;
-
-app.use('/', baseRouter);
-
-const { BAD_REQUEST } = StatusCodes;
-app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
-  // TODO: Refactor later that sends back more than just a 400
-  // Because not all requests that fail are the fault of the client
-  console.log('Our custom error handler');
-  log.error(err);
-  res.status(BAD_REQUEST).json({
-    error: err.message,
-  });
-
-  next(err);
-});
-
 app.listen(port, () => {
   console.log(`Server has started listining on port ${port}`);
 });
-
-app.get('/json', (req, res) => {
-  console.log('Our callback was invoked!');
-  res.json({ data: 'This is sending back JSON' });
-});
-const { BAD_REQUEST } = StatusCodes;
-app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
-  console.log('Our custom callback was invoked');
-  log.error(err, true);
-  res.status(BAD_REQUEST).json({
-    error: err.message,
-  });
-  next(err);
-});
-app.listen(port, () => {
-  console.log(`Server has started listining on port ${port}`);
-});
-const Router = Returj')
