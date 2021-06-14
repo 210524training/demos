37bc672b9653363555430fe1196c069b6e32a1b5
dixond1097 <79214021+dixond1097@users.noreply.github.com>
2021-06-14 18:42:17 -0400

    Changed a couple things

---
+++ week3/express/grubdash/src/ClickerFunction.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+type Prop = {
+    amount?: number;
+}
+const ClickerFunction: React.FC<Prop> = (props) => (
+  <div>
+
+  </div>
+);
+}
+
+export default ClickerFunction;
