d50ed35c17653cbfe452e9c19ae1bc002898cbcb
IkenoXamos <moberlies1337@gmail.com>
2021-06-17 00:55:02 -0400

    Refactor some styles and add missing JS plugin for bootstrap

--- week4/grubdash-react/src/home-page/HomePage.tsx
+++ week4/grubdash-react/src/home-page/HomePage.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import Features from '../features/Features';
 import './HomePage.css';
-import img1 from '../assets/scooter.svg';
-import img2 from '../assets/iphone.svg';
-import img3 from '../assets/storefront.svg';
+import scooter from '../assets/scooter.svg';
+import iphone from '../assets/iphone.svg';
+import storefront from '../assets/storefront.svg';
 
 type Props = {
 }
@@ -13,32 +13,33 @@ const HomePage: React.FC<Props> = (props) => {
     <>
       <div className="banner">
         <div>
-          <input className="topButton" value="Sign-Up" type='submit'/>
           <h1 className='whiteText'>GrubDash</h1><br/>
         </div>
         <br/>
         <h2 className='whiteText'>Restaurants and more, </h2>
         <h2 className='whiteText'>delivered to your door</h2>
         <br/>
-        <input className="input1"/> <input value="Go" className="input2" type='submit'/>
+        <input className="input1" placeholder="Your Address"/> <input value="Search" className="input2" type='submit'/>
       </div>
-      <div className="row">
-        <div className="col-sm-4">
-          <div className='imgbox'>
-            <img src={ img1 } alt='somthing' className="image1"/>
-            <h3>Become a Dasher</h3>
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-sm-4">
+            <div className='imgbox'>
+              <img src={ scooter } alt='somthing' className="image1"/>
+              <h3>Become a Dasher</h3>
+            </div>
           </div>
-        </div>
-        <div className="col-sm-4">
-          <div className='imgbox'>
-            <img src={ img2 } alt='somthing' className="image1"/>
-            <h3>Try the app</h3>
-          </div>
-        </div>       
-        <div className="col-sm-4">
-          <div className='imgbox'>
-            <img src={ img3 } alt='somthing' className="image1"/>
-            <h3>View Restuarants</h3>
+          <div className="col-sm-4">
+            <div className='imgbox'>
+              <img src={ iphone } alt='somthing' className="image1"/>
+              <h3>Try the app</h3>
+            </div>
+          </div>       
+          <div className="col-sm-4">
+            <div className='imgbox'>
+              <img src={ storefront } alt='somthing' className="image1"/>
+              <h3>View Restaurants</h3>
+            </div>
           </div>
         </div>
       </div>
@@ -46,7 +47,6 @@ const HomePage: React.FC<Props> = (props) => {
         <Features />
       </div>
       <div className="footer">
-
       </div>
     </>
   );
--- week4/grubdash-react/src/navbar/Navbar.tsx
+++ week4/grubdash-react/src/navbar/Navbar.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import './Navbar.css';
 
 type Props = {
 
@@ -8,20 +7,26 @@ type Props = {
 
 const Navbar: React.FC<Props> = (props) => {
   return (
-    <>
-      <nav className="navbar navbar-nav navbar-expand-md navbar-light bg-light nav-float">
-        <div id="nav" className="container-fluid">
-            <NavLink className="navbar-brand" to="/">GrubDash</NavLink>
-            <div className="collapse navbar-collapse" id="navbarSupportedContent">
-              <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                <li className="nav-item">
-                  <NavLink className="nav-link" to="/restaurants">Restaurants</NavLink>
-                </li>
-              </ul>
-            </div>
+    <nav className="navbar navbar-expand-md navbar-light bg-light fixed-top">
+      <div id="nav" className="container-fluid">
+        <NavLink className="navbar-brand" to="/">GrubDash</NavLink>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarCollapse">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/restaurants">Restaurants</NavLink>
+            </li>
+          </ul>
+          <ul className="navbar-nav ms-auto">
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/register">Register</NavLink>
+            </li>
+          </ul>
         </div>
-      </nav>
-    </>
+      </div>
+    </nav>
   )
 }
 
