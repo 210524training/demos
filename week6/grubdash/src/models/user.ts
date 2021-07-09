e800c711c503d02d9c7b09124bb6150055015f37
IkenoXamos <moberlies1337@gmail.com>
2021-07-09 17:45:16 -0400

    Add Grubdash SQL Schema and new SQL Repositories

--- week6/grubdash/src/models/user.ts
+++ week6/grubdash/src/models/user.ts
@@ -1,13 +1,11 @@
-import { v4 as uuidv4 } from 'uuid';
-
 export default class User {
   constructor(
     public username: string,
     public password: string,
     public role: Role = 'Customer',
-    public address: string = '',
-    public phoneNumber: string = '',
-    public id: string = uuidv4(),
+    public address?: string,
+    public phoneNumber?: string,
+    public id?: string,
   ) {}
 }
 
